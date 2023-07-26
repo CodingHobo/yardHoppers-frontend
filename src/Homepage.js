5faa3af0e93997fea2c58775082538d13a44ae6d
Tanya Shylock <tourtanya@gmail.com>
2023-07-25 19:32:38 -0600

    edit Provider value to {currUser}

--- src/Homepage.js
+++ src/Homepage.js
@@ -14,23 +14,19 @@ import logo from "./logo.png";
 
 function Homepage() {
   const { currUser } = useContext(userContext);
-  const isLoggedIn = !!currUser; // Check if currUser exists (i.e., user is logged in)
 
   return (
     <div className="home">
-      {!isLoggedIn && (
-        <div>
-          <h4>Thanks for checking out my demo!</h4>
-          <h5>
-            Proceed to the <a href="/login">Login</a> Page for instructions on navigating the
-            site.
-          </h5>
-        </div>
+      {!currUser && (
+      <div>
+        <h4>Thanks for checking out my demo!</h4>
+        <h5>Proceed to the <a href="/login">Login</a> Page for instructions on navigating the site.</h5>
+      </div>
       )}
       <header className="home-header">
         <img src={logo} className="home-logo" alt="logo" />
         <div className="title">
-          {isLoggedIn && <h3>Welcome, {currUser.username}!</h3>}
+        {currUser && <h3>Welcome, {currUser.username}!</h3>}
           <h1>Yard Hoppers</h1>
           <h4>Get on my lawn!</h4>
         </div>
